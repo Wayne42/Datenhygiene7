eac4642c8934cfb21a2c34ff162a45226d23a6a8
Ferhat Tohidi Far <ferhat064@hotmail.de>
2019-01-31 00:11:51 +0100

    asdf

--- app/src/main/java/tf/ferhat/datenhygiene7/MainActivity.java
+++ app/src/main/java/tf/ferhat/datenhygiene7/MainActivity.java
@@ -1,49 +1,44 @@
 package tf.ferhat.datenhygiene7;
 
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageInfo;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.TextView;
-import android.content.pm.PackageManager;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        StringBuffer appNameAndPermissions = new StringBuffer();
-        PackageManager pm = getPackageManager();
-
-        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
-
-        for (ApplicationInfo applicationInfo : packages) {
-            Log.d("test", "App: " + applicationInfo.name + " Package: " + applicationInfo.packageName);
-            try {
-                PackageInfo packageInfo = pm.getPackageInfo(applicationInfo.packageName, PackageManager.GET_PERMISSIONS);
-                appNameAndPermissions.append(packageInfo.packageName + "*******:\n");
-
-                //Get Permissions
-                String[] requestedPermissions = packageInfo.requestedPermissions;
-                if (requestedPermissions != null) {
-                    for (int i = 0; i < requestedPermissions.length; i++) {
-                        Log.d("test", requestedPermissions[i]);
-                        appNameAndPermissions.append(requestedPermissions[i] + "\n");
-                    }
-                    appNameAndPermissions.append("\n");
-                }} catch(PackageManager.NameNotFoundException e){
-                e.printStackTrace();
-            }
-        }
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 }
