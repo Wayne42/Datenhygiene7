9125c7a5e4529515de7c48502db4fe9a671219c5
Ferhat Tohidi Far <ferhat064@hotmail.de>
2019-01-31 00:06:07 +0100

    asdf

--- app/src/main/java/tf/ferhat/datenhygiene7/MainActivity.java
+++ app/src/main/java/tf/ferhat/datenhygiene7/MainActivity.java
@@ -46,10 +46,4 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 }
